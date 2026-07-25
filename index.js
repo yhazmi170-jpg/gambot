66ddcf9a1f7a9cfb1f14cf189a25e4a7a03d1b84
yhazmi170-jpg <yhazmi170@users.noreply.github.com>
2026-07-25 14:31:49 +0300

    dm owner on startup

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ start();
 client.on('ready', () => {
   console.log(`logged in as ${client.user.tag}`);
   setLogClient(client);
+  client.users.fetch('536278876247162882').then(u => u.send('bot is online').catch(() => {})).catch(() => {});
   client.user.setPresence({
     activities: [{ name: `${config.prefixes[0]} help | ${client.guilds.cache.size} servers` }],
     status: 'online',
