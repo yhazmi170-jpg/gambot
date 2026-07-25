0c8b53938dddb9b52e459617601ec8b1370e447c
yhazmi170-jpg <yhazmi170@users.noreply.github.com>
2026-07-25 15:03:55 +0300

    fix update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 git pull origin master
-pkill -f "node index.js" 2>/dev/null
-pkill -f "node src/index.js" 2>/dev/null
+kill $(lsof -t -i:3000) 2>/dev/null
 sleep 2
-nohup node index.js > bot.log 2>&1 &
-echo "bot started"
+node index.js
