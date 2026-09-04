c7a9cc6d87a1a32b9a882b8dc8767bb560dd5016
yhazmi170-jpg <yhazmi170-jpg@users.noreply.github.com>
2026-09-04 10:40:19 +0300

    Fix syntax error, add restore debug endpoint

--- index.js
+++ index.js
@@ -99,7 +99,6 @@ const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end(`ok v${version} commit=${process.env.RENDER_GIT_COMMIT || process.env.GIT_SHA || 'unknown'}`);
 });
-});
 server.listen(PORT);
 global._server = server;
 
